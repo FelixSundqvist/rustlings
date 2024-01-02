6f39b0a73e8abd99cdeb21d55eceec8b4022e031
Felix Sundqvist <felix.sundqvist@sitedrive.com>
2024-01-02 18:48:27 +0200

    fix: tests

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,8 +11,6 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -35,14 +33,19 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.into_iter().fold(0, |count, (key, val)| {
+        if val == &value {
+            return count + 1;
+        }
+        count
+    })
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
     let mut count = 0;
     for map in collection {
-        for val in map.values() {
-            if val == &value {
+        for &val in map.values() {
+            if val == value {
                 count += 1;
             }
         }
@@ -51,10 +54,7 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
 }
 
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
-    // collection is a slice of hashmaps.
-    // collection = [{ "variables1": Complete, "from_str": None, ... },
-    //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection.iter().map(|item| item.iter().filter(|(_, &val)| val == value).count()).sum()
 }
 
 #[cfg(test)]
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,32 +20,41 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
     Append(usize),
 }
 
-mod my_module {
+pub mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Append(size) => {
+                    let bar = "bar".repeat(*size);
+                    output.push(string.to_owned() + &bar);
+                }
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                }
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+            }
         }
+
         output
     }
 }
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
@@ -62,3 +71,4 @@ mod tests {
         assert_eq!(output[3], "barbarbarbarbarbar");
     }
 }
+
